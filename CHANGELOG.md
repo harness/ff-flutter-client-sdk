858b3c65c2a0bfea4aa69671ce8c67807bc4846e
Stefan Tanovic <stefan.tanovic@harness.io>
2021-03-29 15:10:04 +0200

    Updating documentation and CHANGELOG file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+This is first pubic release of Feature Flags client Flutter client SDK.
+
+New changes:
+* Support for initialization and configuration of SDK client
+* Feature flag evaluations
+* Option to observe for changes of feature flags
--- lib/CfClient.dart
+++ lib/CfClient.dart
@@ -28,6 +28,9 @@ class EvaluationRequest {
   }
 }
 
+/// Class representing a single evaluation response. It carries information about
+/// changed evaluation's id and its value. The value type is dynamic and it is
+/// SDK's end-user responsibility to know what type is used for given id
 class EvaluationResponse {
   String evaluationId;
   dynamic evaluationValue;
@@ -35,15 +38,43 @@ class EvaluationResponse {
   EvaluationResponse(this.evaluationId, this.evaluationValue);
 }
 
+/// Class used in [CfEventsListener] as possible event type that can be triggered by SDK.
 enum EventType {
+  /// Indicates that realtime evaluation update monitor is started. Has no payload.
   SSE_START,
+
+  /// Indicates that realtime evaluation update monitor is ended. Has no payload.
   SSE_END,
+
+  /// Evaluation list has been reloaded due to polling mechanism. The payload is
+  /// list of loaded evaluation.
+  /// See [EvaluationResponse]
   EVALUATION_POLLING,
+
+  /// A single evaluation has been changed. The payload is an instance of [EvaluationResponse].
   EVALUATION_CHANGE
 }
 
+/// Type alias used in [CfClient.registerEventsListener] to receive events from SDK. It has two parameters:
+/// - data: dynamic type since different events may carry different response
+/// - eventType: instance of [EventType] class
+/// For full list of possible event and response types, see [EventType] class
 typedef void CfEventsListener(dynamic data, EventType eventType);
 
+///
+/// Main SDK client used for SDK initialization and flags evaluation. The initialization is done
+/// with help of [CfConfigurationBuilder].
+/// Example:
+///```
+/// final conf = CfConfigurationBuilder()
+///     .setStreamEnabled(true)
+///     .setPollingInterval(60) //time in seconds (minimum value is 60)
+///     .build();
+/// final target = CfTargetBuilder().setIdentifier(name).build();
+///
+/// final res = await CfClient.initialize(apiKey, conf, target);
+///
+///```
 class CfClient {
   static const MethodChannel _channel = const MethodChannel('ff_flutter_client_sdk');
   static const MethodChannel _hostChannel = const MethodChannel('cf_flutter_host');
@@ -87,6 +118,8 @@ class CfClient {
     }
   }
 
+  /// Initializes the SDK client with provided API key, configuration and target. Returns information if
+  /// initialization succeeded or not
   static Future<InitializationResult> initialize(String apiKey, CfConfiguration configuration, CfTarget target) async {
     _hostChannel.setMethodCallHandler(_hostChannelHandler);
 
@@ -99,14 +132,17 @@ class CfClient {
     return new Future(() => InitializationResult(initialized));
   }
 
+  /// Performs string evaluation for given evaluation id. If no such id is present, the default value will be returned.
   static Future<String> stringVariation(String evaluationId, String defaultValue) async {
     return _sendMessage('stringVariation', new EvaluationRequest(evaluationId, defaultValue));
   }
 
+  /// Performs boolean evaluation for given evaluation id. If no such id is present, the default value will be returned.
   static Future<bool> boolVariation(String evaluationId, bool defaultValue) async {
     return _sendMessage('boolVariation', new EvaluationRequest(evaluationId, defaultValue));
   }
 
+  /// Performs evaluation for given evaluation id with double value. If no such id is present, the default value will be returned.
   static Future<double> numberVariation(String evaluationId, double defaultValue) async {
     return _sendMessage('numberVariation', new EvaluationRequest(evaluationId, defaultValue));
   }
@@ -119,15 +155,19 @@ class CfClient {
     return _channel.invokeMethod(messageType, evaluationRequest.toMap());
   }
 
+  /// Register a listener for different types of events. Possible types are based on [EventType] class
   static Future<void> registerEventsListener(CfEventsListener listener) async {
     _listenerSet.add(listener);
     return _channel.invokeMethod('registerEventsListener');
   }
-  
+
+  /// Removes a previously-registered listener from internal collection of listeners. From this point, provided
+  /// listener will not receive any events triggered by SDK
   static Future<void> unregisterEventsListener(CfEventsListener listener) async {
     _listenerSet.remove(listener);
   }
 
+  /// Client's method to deregister and cleanup internal resources used by SDK
   static Future<void> destroy() async {
     _listenerSet.clear();
     return _channel.invokeMethod('destroy');
