639a38fb513a0d4b7323b2b00b25f644245e5f03
Erdi Rowlands <erdi.rowlands@harness.io>
2023-01-23 19:30:35 +0000

    FFM-6395 Add logging to SDK

--- lib/CfClient.dart
+++ lib/CfClient.dart
@@ -2,12 +2,16 @@ library ff_flutter_client_sdk;
 
 import 'dart:async';
 import 'dart:collection';
+import 'package:logging/logging.dart';
 import 'package:flutter/services.dart';
 
 
 part 'CfTarget.dart';
 
 part 'CfConfiguration.dart';
+
+final log = Logger('CFClientLogger');
+
 
 class InitializationResult {
   InitializationResult(bool value) {
@@ -134,16 +138,22 @@ class CfClient {
   /// initialization succeeded or not
   Future<InitializationResult> initialize(String apiKey,
       CfConfiguration configuration, CfTarget target) async {
+    Logger.root.level = Level.SEVERE; // defaults to Level.INFO
+    Logger.root.onRecord.listen((record) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    });
     _hostChannel.setMethodCallHandler(_hostChannelHandler);
-
-    // TODO - we need a try-catch here on PlatformException in order to retry.
-    bool initialized = await _channel.invokeMethod('initialize', {
+    bool initialized = false;
+    try {
+    initialized = await _channel.invokeMethod('initialize', {
       'apiKey': apiKey,
       'configuration': configuration._toCodecValue(),
       'target': target._toCodecValue()
-    });
-
-    return new Future(() => InitializationResult(initialized));
+    }); } on PlatformException catch(e) {
+      // For now just log the error. In the future, we should add retry and backoff logic.
+      log.severe(e.message, [e.details]);
+      return new Future(() => InitializationResult(initialized));
+    }
 }
 
   /// Performs string evaluation for given evaluation id. If no such id is present, the default value will be returned.
