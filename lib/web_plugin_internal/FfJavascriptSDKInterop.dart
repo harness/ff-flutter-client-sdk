5001537e33300ba2a1dfda4c83348d9e98922896
Erdi Rowlands <erdi.rowlands@harness.io>
2023-08-08 17:34:54 +0100

    FFM-8961 Use iife distribution name

--- lib/web_plugin_internal/FfJavascriptSDKInterop.dart
+++ lib/web_plugin_internal/FfJavascriptSDKInterop.dart
@@ -3,5 +3,7 @@ library rudder_analytics.js;
 
 import 'package:js/js.dart';
 
-@JS("harnessFfFlutterClientSDK.initialize")
+// HarnessFFSDK is the global name that the JavaScript SDK sets in its iife distribution, which
+// we ask users to import. 
+@JS("HarnessFFSDK.initialize")
 external load(String writeKey, String dataPlaneUrl, dynamic options);
