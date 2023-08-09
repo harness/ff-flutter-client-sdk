6d02d412ec92ce7930718d5195d0eec382b66d7b
Erdi Rowlands <erdi.rowlands@harness.io>
2023-08-09 13:54:28 +0100

    FFM-8961 Tidy up for post spike

--- lib/FfFlutterClientSDKWebPlugin.dart
+++ lib/FfFlutterClientSDKWebPlugin.dart
@@ -2,10 +2,12 @@
 library static_interop;
 
 import 'dart:async';
+import 'dart:js_util';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:js/js.dart';
 import 'package:logging/logging.dart';
+import 'web_plugin_internal//FfJavascriptSDKInterop.dart' as webJs;
 
 // @JS('FFJavaScriptClientSDK')
 // @staticInterop
@@ -21,13 +23,13 @@ import 'package:logging/logging.dart';
 final log = Logger('FfFlutterClientSdkWebPluginLogger');
 
 class FfFlutterClientSdkWebPlugin {
-
   // The instance of the wrapper around the JS SDK
   // static final harness = FFJavaScriptClientSDK();
 
   // This channel is used to send JavaScript SDK events to the Flutter
   // SDK Code.
-  static const MethodChannel _hostChannel = const MethodChannel('cf_flutter_host');
+  static const MethodChannel _hostChannel =
+      const MethodChannel('cf_flutter_host');
 
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
@@ -47,10 +49,20 @@ class FfFlutterClientSdkWebPlugin {
     switch (call.method) {
       case 'initialize':
         // return harness.initialize(call.arguments["apiKey"], call.arguments["target"], call.arguments["options"]);
-    //   case 'registerEvent':
-    //     return harness.registerEvent(call.arguments["eventType"], allowInterop((dynamic event) {
-    //       // Process JavaScript call here
-    //     }));
+        //   case 'registerEvent':
+        //     return harness.registerEvent(call.arguments["eventType"], allowInterop((dynamic event) {
+        //       // Process JavaScript call here
+        //     }));
+        final response =
+            webJs.initialize("2809ada1-73ae-4008-9c75-f0b9e7sedd797", {
+          'name': 'Sample Name',
+          'identifier': 12345,
+          'isAnonymous': false,
+          'attributes': {'key1': 'value1', 'key2': 'value2'}
+        }, {});
+        var propertyValue = getProperty(response, 'on');
+        print(propertyValue);
+        return true;
     }
   }
 
--- lib/web_plugin_internal/FfJavascriptSDKInterop.dart
+++ lib/web_plugin_internal/FfJavascriptSDKInterop.dart
@@ -1,9 +1,10 @@
 @JS()
-library rudder_analytics.js;
+library harness_javascript_sdk.js;
 
+import 'package:ff_flutter_client_sdk/CfClient.dart';
 import 'package:js/js.dart';
 
 // HarnessFFSDK is the global name that the JavaScript SDK sets in its iife distribution, which
-// we ask users to import. 
+// we ask users to import.
 @JS("HarnessFFSDK.initialize")
-external load(String writeKey, String dataPlaneUrl, dynamic options);
+external dynamic initialize(String apiKey, dynamic target, dynamic options);
