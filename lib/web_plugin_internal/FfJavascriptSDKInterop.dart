6c3ef225142f0ea924f8e0b6db08907852f3fe98
Erdi Rowlands <erdi.rowlands@harness.io>
2023-08-09 17:14:42 +0100

    FFM-8961 Use static class constants instead of constant string literals

--- lib/web_plugin_internal/FfJavascriptSDKInterop.dart
+++ lib/web_plugin_internal/FfJavascriptSDKInterop.dart
@@ -3,28 +3,25 @@ library harness_javascript_sdk.js;
 
 import 'package:js/js.dart';
 
-// The window reference to the JavaScript SDK, which we use before it's
-// initialized.
-const sdkWindowReference = 'HarnessFFSDK';
-// Exposed by the JavaScript SDK
-const sdkInitialize = 'initialize';
-const sdkEvent = 'Event';
+class SDKReferences {
+  static const window = 'HarnessFFSDK';
+  static const initialize = 'initialize';
+  static const event = 'Event';
+}
 
-// Once we've initialized the Client, we set it as a property on the window using
-// this reference
-const clientWindowReference = 'cfClient';
-// Exposed by the initialized client instance
-const clientOn = 'on';
-const clientOff = 'off';
-const clientVariation = 'variation';
-const clientClose = 'close';
+class ClientReferences {
+  static const window = 'cfClient';
+  static const on = 'on';
+  static const off = 'off';
+  static const variation = 'variation';
+  static const close = 'close';
+}
 
-// HarnessFFSDK is the global name that the JavaScript SDK sets in its iife distribution, which
-// we ask users to import.
-@JS("$sdkWindowReference.$sdkInitialize")
+// Usage in annotations:
+@JS("${SDKReferences.window}.${SDKReferences.initialize}")
 external dynamic initialize(
     String apiKey, Map<String, dynamic> target, Map<String, dynamic> options);
 
-@JS('$clientWindowReference.$clientOn')
+@JS('${ClientReferences.window}.${ClientReferences.on}')
 external dynamic on(
     String apiKey, Map<String, dynamic> target, Map<String, dynamic> options);
