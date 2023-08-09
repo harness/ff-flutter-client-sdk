3f04b45759f6306a547410fa0501fde0dc6cece8
Erdi Rowlands <erdi.rowlands@harness.io>
2023-08-09 15:48:15 +0100

    FFM-8961 Use explicit type in interop
    FFM-8961 Use explicit type in interop

--- lib/web_plugin_internal/FfJavascriptSDKInterop.dart
+++ lib/web_plugin_internal/FfJavascriptSDKInterop.dart
@@ -1,10 +1,9 @@
 @JS()
 library harness_javascript_sdk.js;
 
-import 'package:ff_flutter_client_sdk/CfClient.dart';
 import 'package:js/js.dart';
 
 // HarnessFFSDK is the global name that the JavaScript SDK sets in its iife distribution, which
 // we ask users to import.
 @JS("HarnessFFSDK.initialize")
-external dynamic initialize(String apiKey, dynamic target, dynamic options);
+external dynamic initialize(String apiKey, Map<String, dynamic> target, Map<String, dynamic> options);
