726c97bc03bf27e80a58f829931ba7c5dd47c269
Erdi Rowlands <erdi.rowlands@harness.io>
2023-08-22 18:13:36 +0100

    FFM-8962 Don't store init result in var

--- lib/FfFlutterClientSDKWebPlugin.dart
+++ lib/FfFlutterClientSDKWebPlugin.dart
@@ -113,7 +113,7 @@ class FfFlutterClientSdkWebPlugin {
     final initErrorCallback = (dynamic error) {
       // Same as above, defensive check.
       if (!_initializationResult.isCompleted) {
-        log.severe("Failed to initialize: " + (error?.toString() ?? 'Auth error was empty'));
+        log.severe("FF SDK failed to initialize: " + (error?.toString() ?? 'Auth error was empty'));
         _initializationResult.complete(false);
       } else {
         log.fine(
