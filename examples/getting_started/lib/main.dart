4ae103db7218e28fe46f5969abceac7c7f229d88
Erdi Rowlands <erdi.rowlands@harness.io>
2023-09-11 17:29:41 +0100

    FFM-8963 Update sample app

--- examples/getting_started/lib/main.dart
+++ examples/getting_started/lib/main.dart
@@ -2,19 +2,19 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:ff_flutter_client_sdk/CfClient.dart';
+import 'package:logging/logging.dart';
 
 // The SDK API Key to use for authentication.  Configure it when installing the app by setting FF_API_KEY
 // e.g..
 const apiKey = String.fromEnvironment('FF_API_KEY', defaultValue: '');
 
-// flagName is the identifier of the flag to evaluate
-const flagName = String.fromEnvironment('FF_FLAG_NAME', defaultValue: 'harnessappdemodarkmode');
+const boolFlagName = 'boolflag';
+const stringFlagName = "multivariateflag";
+const numberFlagName = "numberflag";
+const jsonFlagName = "jsonflag";
 
-// Setup App
 void main() => runApp(MyApp());
 
-// A simple StatelessWidget that fetches the latest value from the FF Service.
-// Everytime it receives an update the value of the flag is updated.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,7 +28,10 @@ class FlagState extends StatefulWidget {
 }
 
 class _FlagState extends State<FlagState> {
-  bool _flagValue = false;
+  dynamic _boolFlagValue = false;
+  dynamic _stringFlagValue = "off";
+  dynamic _numberFlagValue = 3;
+  dynamic _jsonFlagValue = {};
 
   @override
   void initState() {
@@ -36,7 +39,7 @@ class _FlagState extends State<FlagState> {
 
     // Create Default Configuration for the SDK.  We can use this to disable streaming,
     // change the URL the client connects to etc
-    var conf = CfConfigurationBuilder().build();
+    var conf = CfConfigurationBuilder().setLogLevel(Level.FINE).build();
 
     // Create a target (different targets can get different results based on rules.  This include a custom attribute 'location')
     var target = CfTargetBuilder().setIdentifier("fluttersdk").setName("FlutterSDK").build();
@@ -48,37 +51,78 @@ class _FlagState extends State<FlagState> {
         print("Successfully initialized client");
 
         // Evaluate flag and set initial state
-        CfClient.getInstance().boolVariation(flagName, false).then((value) {
-          print("$flagName: $value");
+        CfClient.getInstance().boolVariation("Adasdas", false).then((value) {
+          print("$_boolFlagValue: $value");
           setState(() {
-            _flagValue = value;
+            _boolFlagValue = value;
+          });
+        });
+
+        // Evaluate flag and set initial state
+        CfClient.getInstance().jsonVariation("Adasdas", {"aaa": "asdsd"}).then((value) {
+          print("$_jsonFlagValue: $value");
+          setState(() {
+            _jsonFlagValue = value;
+          });
+        });
+
+        // Evaluate flag and set initial state
+        CfClient.getInstance().stringVariation("Adasdas", "default").then((value) {
+          print("$_stringFlagValue: $value");
+          setState(() {
+            _stringFlagValue = value;
+          });
+        });
+
+        // Evaluate flag and set initial state
+        CfClient.getInstance().numberVariation("Adasdas", 1).then((value) {
+          print("$_numberFlagValue: $value");
+          setState(() {
+            _numberFlagValue = value;
           });
         });
 
         // Setup Event Handler
-        CfClient.getInstance().registerEventsListener((data, eventType) {
+        listener(data, eventType) {
           print("received event: ${eventType.toString()} with Data: ${data.toString()}");
           switch (eventType) {
-                case EventType.EVALUATION_CHANGE:
-                    String flag = (data as EvaluationResponse).flag;
-                    dynamic value = (data as EvaluationResponse).value;
-                    // If the change concerns the flag we care about, then update the state
-                    if ( flag == flagName ) {
-                        setState(() {
-                           _flagValue = value.toLowerCase() == 'true';
-                        });
-                    }
-                    break;
+            case EventType.EVALUATION_CHANGE:
+              String flag = (data as EvaluationResponse).flag;
+              dynamic value = (data as EvaluationResponse).value;
+              switch(flag) {
+                case boolFlagName:
+                  setState(() {
+                    _boolFlagValue = value;
+                  });
+                  break;
+                case stringFlagName:
+                  setState(() {
+                    _stringFlagValue = value;
+                  });
+                  break;
+                case numberFlagName:
+                  setState(() {
+                    _numberFlagValue = value;
+                  });
+                  break;
+                case jsonFlagName:
+                  setState(() {
+                    _jsonFlagValue = value;
+                  });
+                  break;
+              }
+              break;
+
 
 
           // There's been an interruption in the SSE but which has since resumed, which means the
           // cache will have been updated with the latest values, so we can call
           // bool variation to get the most up to date evaluation value.
             case EventType.SSE_RESUME:
-              CfClient.getInstance().boolVariation(flagName, false).then((value) {
-                print("$flagName: $value");
+              CfClient.getInstance().boolVariation(boolFlagName, false).then((value) {
+                print("$boolFlagName: $value");
                 setState(() {
-                  _flagValue = value;
+                  _boolFlagValue = value;
                 });
               });
               break;
@@ -86,7 +130,9 @@ class _FlagState extends State<FlagState> {
             default:
               break;
           }
-        });
+        }
+        CfClient.getInstance().registerEventsListener(listener);
+        // CfClient.getInstance().unregisterEventsListener(listener);
       }
     });
   }
@@ -99,7 +145,24 @@ class _FlagState extends State<FlagState> {
       ),
       body: Container(
         child: Center(
-          child: new Text("${flagName} : ${_flagValue}", style: TextStyle(fontSize: 25)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text("$boolFlagName : $_boolFlagValue", style: const TextStyle(fontSize: 25)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text("$stringFlagName : $_stringFlagValue", style: const TextStyle(fontSize: 25)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text("$numberFlagName : $_numberFlagValue", style: const TextStyle(fontSize: 25)),
+              ),
+              Text("$jsonFlagName : $_jsonFlagValue", style: const TextStyle(fontSize: 25)),
+            ],
+          ),
         ),
       ),
     );
