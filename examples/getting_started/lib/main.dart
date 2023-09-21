85786f5b6b837bed87526a8a49ead9985295bf43
Erdi Rowlands <erdirowlands@gmail.com>
2023-09-21 14:22:53 +0100

    FFM-8967 Update sample app to work with web

--- examples/getting_started/lib/main.dart
+++ examples/getting_started/lib/main.dart
@@ -16,9 +16,6 @@ const jsonFlagName = "jsonflag";
 
 
 void main() async {
-  // To load the .env file contents into dotenv.
-  // NOTE: fileName defaults to .env and can be omitted in this case.
-  // Ensure that the filename corresponds to the path in step 1 and 2.
   await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
