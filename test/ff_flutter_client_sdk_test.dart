03bd0cb1ad2e710a2cb0662293d9be283d90a695
Erdi Rowlands <erdirowlands@gmail.com>
2023-09-22 16:53:50 +0100

    FFM-8967 Update test

--- test/ff_flutter_client_sdk_test.dart
+++ test/ff_flutter_client_sdk_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ff_flutter_client_sdk/CfClient.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('ff_flutter_client_sdk');
