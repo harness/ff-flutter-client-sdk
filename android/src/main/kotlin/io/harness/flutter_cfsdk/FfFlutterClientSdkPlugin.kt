79070407c2d18b82817d756f58e9dfafe5fefa7e
Erdi Rowlands <erdirowlands@gmail.com>
2024-01-19 16:57:39 +0000

    FFM-10417 Add release mode instructions for Android (#30)

--- android/src/main/kotlin/io/harness/flutter_cfsdk/FfFlutterClientSdkPlugin.kt
+++ android/src/main/kotlin/io/harness/flutter_cfsdk/FfFlutterClientSdkPlugin.kt
@@ -3,26 +3,21 @@ package io.harness.flutter_cfsdk
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
-import androidx.annotation.NonNull
-import com.google.gson.JsonObject
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.harness.cfsdk.AndroidSdkVersion.ANDROID_SDK_VERSION
 import io.harness.cfsdk.CfClient
 import io.harness.cfsdk.CfConfiguration
 import io.harness.cfsdk.cloud.model.Target
+import io.harness.cfsdk.cloud.openapi.client.model.Evaluation
 import io.harness.cfsdk.cloud.sse.EventsListener
 import io.harness.cfsdk.cloud.sse.StatusEvent
 import org.json.JSONArray
 import org.json.JSONObject
-import java.util.concurrent.*;
-import io.harness.cfsdk.AndroidSdkVersion.ANDROID_SDK_VERSION;
-import io.harness.cfsdk.cloud.openapi.client.model.Evaluation
-import kotlin.Exception
+import java.util.concurrent.Executors
 
 /** FfFlutterClientSdkPlugin */
 class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
@@ -47,7 +42,7 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ff_flutter_client_sdk")
         hostChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "cf_flutter_host")
         application = flutterPluginBinding.applicationContext as Application
@@ -76,9 +71,9 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
         return builder
     }
 
-    private fun invokeInitialize(@NonNull call: MethodCall, @NonNull result: Result) {
+    private fun invokeInitialize(call: MethodCall, result: Result) {
 
-        println("Using Android SDK version ${ANDROID_SDK_VERSION}")
+        println("Using Android SDK version $ANDROID_SDK_VERSION")
 
         val config: Map<String, Any>? = call.argument("configuration")
         val key: String? = call.argument("apiKey")
@@ -109,7 +104,7 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
 
                         Handler(Looper.getMainLooper()).post {
 
-                            result.success(execResult.isSuccess())
+                            result.success(execResult.isSuccess)
                         }
 
                     }
@@ -129,7 +124,7 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
     private fun evaluationToMap(evaluation: Evaluation): Map<String, Any> {
         return mutableMapOf<String, Any>().apply {
             this["flag"] = evaluation.flag
-            this["value"] = evaluation.getValue()
+            this["value"] = evaluation.value
             this["kind"] = evaluation.kind
         }
     }
@@ -198,29 +193,29 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
         return Pair(flag, defaultValue)
     }
 
-    private fun invokeStringEvaluation(@NonNull call: MethodCall): String {
+    private fun invokeStringEvaluation(call: MethodCall): String {
         val args = extractEvaluationArgs(call)
         return CfClient.getInstance().stringVariation(args.first, args.second as String)
     }
 
-    private fun invokeBoolEvaluation(@NonNull call: MethodCall): Boolean {
+    private fun invokeBoolEvaluation(call: MethodCall): Boolean {
         val args = extractEvaluationArgs(call)
         return CfClient.getInstance().boolVariation(args.first, args.second as Boolean)
     }
 
-    private fun invokeNumberEvaluation(@NonNull call: MethodCall): Number {
+    private fun invokeNumberEvaluation(call: MethodCall): Number {
         val args = extractEvaluationArgs(call)
-        println("extracting number argument ${args.second ?: ""}");
+        println("extracting number argument ${args.second ?: ""}")
         val number: Number? = args.second as Number?
         return CfClient.getInstance().numberVariation(args.first, number?.toDouble() ?: 0.0)
     }
 
     private fun invokeDestroy() {
         listener = null
-        CfClient.getInstance().destroy()
+        CfClient.getInstance().close()
     }
 
-    private fun invokeJsonEvaluation(@NonNull call: MethodCall): Map<String, Any?>? {
+    private fun invokeJsonEvaluation(call: MethodCall): Map<String, Any?>? {
 
         val args = extractEvaluationArgs(call)
         return try {
@@ -303,7 +298,7 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
 
         executor.execute {
 
@@ -382,7 +377,7 @@ class FfFlutterClientSdkPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 }
---
+++ examples/getting_started/android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Keep the Feature Flags Android SDK which the Android plugin uses to ensure the Feature Flags
+# Flutter SDK works as intended.
+-keep class io.harness.cfsdk.** { *; }
